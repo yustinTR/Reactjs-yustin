5822a2f5244c745c904ff9e4f7493858700c3554
yustin troost <yustin.troost@finalist.nl>
2023-03-21 14:14:08 +0100

    added material ui & some styling

--- reactjs-yustin/src/App.js
+++ reactjs-yustin/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import PortfolioItemList from "./components/PortfolioItemList";
+import MyComponent from "./components/PortfolioFeaturedItem";
+import Grid from '@mui/material/Grid';
+
 
 function App() {
   return (
     <div className="App">
       <main>
-        <PortfolioItemList/>
+          <MyComponent/>
+          <Grid container spacing={2}>
+            <PortfolioItemList/>
+          </Grid>
       </main>
     </div>
   );
